8cc535d2b6f23179521149d5e76f2f30fa8b245e
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-05-08 21:34:45 -0300

    deployed task history

--- sources/JS/scripts_todolist.js
+++ sources/JS/scripts_todolist.js
@@ -3,11 +3,50 @@
 const todoInput = document.getElementById("ipt-txt-todo");
 const butaoInsertTask = document.querySelector("#btn-insert-task");
 const btnDeleteAll = document.querySelector("button.btn-delete-all-tasks");
-const ulDasLi = document.querySelector("ul.tasks");
+const ulDasLiPrincipal = document.querySelector("ul.tasks");
+const ulDasLiHistory = document.querySelector("ul.tasks-history");
 const butaoDeleteTask = document.getElementById("btn-task-delete");
 let yarraDosItens = [];
+let yarraDoAside = [];
 
-function jogarItensNoArray() {
+function mostrarTarefas() {
+  let newLi = "";
+  yarraDosItens.forEach((item, index) => {         
+    // item e index já são os parametros da arrow function '() => {}'
+
+    // Valores padrão do forEach (valor atual, index, array completo(no caso não usado));
+    newLi += `<li class="task-li-${item.status && "done"}">
+        ${item.task}
+        <input type="button" value="&check;" class="btn-task" id="btn-task-done" title="Done" onclick="doneTask(${index})">
+        
+        <input type="button" value="X" class="btn-task" id="btn-task-delete" title="Delete" onclick="deleteTask(${index})">
+        </li>`;
+
+        // 'item' é o objeto yarraDosItens e 'task' e 'status' são as propriedades do objeto yarraDosItens ;)
+        // E o Index é o index mesmo :)
+  });
+
+  ulDasLiPrincipal.innerHTML = newLi;
+  // console.table(yarraDosItens);
+
+  localStorage.setItem("lista", JSON.stringify(yarraDosItens));
+};
+
+function mostrarHistorico() {
+  let newLiHistory = "";
+
+  yarraDoAside.forEach((item) => {
+    newLiHistory += `
+    <li class = "li-history">
+    <p class="item-history">${item.task}</p>
+    </li>`
+  });
+
+  ulDasLiHistory.innerHTML = newLiHistory;
+  localStorage.setItem("History", JSON.stringify(yarraDoAside));
+}
+
+function jogarItensNoArrayPrincipal() {
   yarraDosItens.push({
     task: todoInput.value,
     status: false,
@@ -20,8 +59,10 @@ function jogarItensNoArray() {
 
 function deleteAllTasks() {
   yarraDosItens.splice(0, yarraDosItens.length);
+
   mostrarTarefas();
-}
+  // mostrarHistorico();
+};
 
 function doneTask(index) {
   yarraDosItens[index].status = !yarraDosItens[index].status;
@@ -29,39 +70,29 @@ function doneTask(index) {
   mostrarTarefas();
 }
 
-function mostrarTarefas() {
-  let newLi = "";
-  yarraDosItens.forEach((item, index) => {
-    // Valores padrão do forEach (valor atual, index, array completo(no caso não usado));
-    newLi += `<li class="task-li-${item.status && "done"}">
-        ${item.task}
-        <input type="button" value="&check;" class="btn-task" id="btn-task-done" title="Done" onclick="doneTask(${index})">
-        
-        <input type="button" value="X" class="btn-task" id="btn-task-delete" title="Delete" onclick="deleteTask(${index})">
-        </li>`;
-  });
-
-  ulDasLi.innerHTML = newLi;
-  // console.table(yarraDosItens);
-
-  localStorage.setItem("lista", JSON.stringify(yarraDosItens));
-}
-
 function deleteTask(index) {
-  yarraDosItens.splice(index, 1);
+  // yarraDosItens.splice(index, 1);
+  const [removedItem] = yarraDosItens.splice(index, 1);
+  yarraDoAside.push(removedItem);
+
+  // console.table(yarraDoAside);
   mostrarTarefas();
+  mostrarHistorico();
 }
 
 function reloadStorage() {
   const yarraLocalStorage = localStorage.getItem("lista");
+  const yarraLocalStorageHistory = localStorage.getItem("History");
 
-  if (yarraLocalStorage) {
+  if (yarraLocalStorage && yarraLocalStorageHistory) {
     yarraDosItens = JSON.parse(yarraLocalStorage);
+    yarraDoAside = JSON.parse(yarraLocalStorageHistory);
   }
 
   mostrarTarefas();
+  mostrarHistorico();
 }
 
 reloadStorage();
 
-butaoInsertTask.addEventListener("click", jogarItensNoArray);
+butaoInsertTask.addEventListener("click", jogarItensNoArrayPrincipal);
