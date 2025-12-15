12244a4387b0e8588e05720e5e2bdec196c75b6b
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-12-14 21:25:26 -0300

    feat: done Pomodoro.

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,36 @@
 import Timer from './modules/timer';
 
 const timerOutput = document.querySelector('.timer');
-const timer = new Timer(30, timerOutput);
-timer.start();
+const selectTimer = document.querySelector(".select-timer");
+let timer = null;
+
+document.addEventListener("click", (e) => {
+
+    if (e.target.classList.contains("btn-start-timer")) {
+        if (!Number(selectTimer.value)) return;
+
+        if (!timer) {
+            timer = new Timer(selectTimer.value, timerOutput);
+        }
+        timer.start();
+    };
+
+    if(e.target.classList.contains("btn-stop")){
+        if(!timer) return;
+        timer.stop();
+    }
+
+    if(e.target.classList.contains("btn-pause")) {
+        if(!timer) return;
+        timer.pause();
+    };
+
+});
+
+// Heard the change in Select Time to update timer.
+document.addEventListener("change", () => {
+    if(!timer) return;
+
+    timer.stop();
+    timer.updateTime(selectTimer.value);
+})
--- public/js/modules/timer.js
+++ public/js/modules/timer.js
@@ -1,27 +1,67 @@
 export default class Timer {
-    constructor(timeInSec, elementOutput) {
-        this.timeInSec = timeInSec * 60;
+    constructor(timeInMin, elementOutput) {
+        this.timeInMin = timeInMin * 60;
+        this.timeInScreen = timeInMin * 60;
         this.elementOutput = elementOutput;
+
+        this.isRunning = false;
+        this.timeOut = null;
     }
 
-    start(){
+    start() {
+        if (this.isRunning) return;
+
+        this.isRunning = true;
         this.timer();
-    }
-
-    timer() {
-            this.elementOutput.innerText = this.formatTime(this.timeInSec);
-            this.timeInSec--;
-
-            if(this.timeInSec > 0){
-                setTimeout(() => this.timer(this.timeInSec), 1000);
-            }
     };
 
-    formatTime(seconds){
+    pause() {
+        if(this.isRunning){
+            this.isRunning = false;
+            clearTimeout(this.timeOut);
+        } else {
+            this.isRunning = true;
+            this.timer();
+        }
+
+        
+    };
+
+    stop() {
+        this.pause();
+        this.timeInScreen = this.timeInMin;
+        this.render();
+    };
+
+    timer() {
+        if (!this.isRunning) return;
+
+        this.render();
+
+        if (this.timeInScreen <= 0) {
+            this.isRunning = false;
+            this.timeInScreen = this.timeInMin;
+            return;
+        }
+        this.timeInScreen--;
+        this.timeOut = setTimeout(() => this.timer(), 1000);
+    };
+
+    render() {
+        this.elementOutput.innerText = this.formatTime(this.timeInScreen);
+    };
+
+    formatTime(seconds) {
         const min = Math.floor(seconds / 60);
         const sec = seconds % 60;
 
         return `${String(min).padStart('2', 0)} : ${String(sec).padStart('2', 0)}`;
+    };
+
+    updateTime(minutes){
+        this.timeInMin = minutes * 60;
+        this.timeInScreen = this.timeInMin;
+        this.render();
     }
 
-}
+};
