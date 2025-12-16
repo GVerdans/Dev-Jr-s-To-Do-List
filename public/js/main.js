9e303b9292e9b1d0f64a0c608228f38586530593
Gabriel Verdan <tahoriemverdan@gmail.com>
2025-12-16 20:51:19 -0300

    feat: init todo list

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,20 @@
-import Timer from './modules/timer';
+import Timer from './modules/Timer';
+import Todo from './modules/Todo';
 
+// Timer Variables
 const timerOutput = document.querySelector('.timer');
 const selectTimer = document.querySelector(".select-timer");
 let timer = null;
 
-document.addEventListener("click", (e) => {
+// Todo Variables
+let todo = null;
+const todoInput = document.querySelector(".inputTxtTask");
+const todoOutput = document.querySelector(".ul-todo-output");
+let list = [];
 
+document.addEventListener("click", (e) => {
+    
+    // -- Timer Button -- //
     if (e.target.classList.contains("btn-start-timer")) {
         if (!Number(selectTimer.value)) return;
 
@@ -24,6 +33,18 @@ document.addEventListener("click", (e) => {
         if(!timer) return;
         timer.pause();
     };
+    // -- End of Timer Events -- //
+
+    // --- Todo Events --- //
+    if(e.target.classList.contains("btnInsertTask")){
+       const task = todoInput.value;
+       if(task == "") return;
+
+       const todo = new Todo(task, list, todoOutput);
+
+       todo.addToList();
+       todoInput.value = "";
+    }
 
 });
 
